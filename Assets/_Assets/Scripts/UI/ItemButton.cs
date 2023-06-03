5d8feca6f08125f61d78b41b04522f536b1ca102
Pyrousha <47787636+Pyrousha@users.noreply.github.com>
2023-06-03 02:06:44 -0500

    Sprite atlasing

---
+++ Assets/_Assets/Scripts/UI/ItemButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemButton : MonoBehaviour
+{
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private TextMeshProUGUI itemQuantity;
+}
